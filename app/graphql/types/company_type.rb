6f4d47fe1ca5a080f183bf9a8d748d259b74e92a
gabrielloyola <gabrielfrml@gmail.com>
2021-08-21 16:37:28 -0300

    Add employee type

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -2,5 +2,7 @@ module Types
   class CompanyType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+
+    field :employees, [EmployeeType], null: true
   end
 end
---
+++ app/graphql/types/employee_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class EmployeeType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :email, String, null: false
+
+    field :company, CompanyType, null: false
+    field :manager, EmployeeType, null: true
+    field :subordinates, [EmployeeType], null: true
+  end
+end
