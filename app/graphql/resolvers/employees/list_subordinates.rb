b23662682a7b5465aafa02a11facc22bff72d765
gabrielloyola <gabrielfrml@gmail.com>
2021-08-21 19:38:56 -0300

    Second level parameter in list_subordinates query

--- app/graphql/resolvers/employees/list_subordinates.rb
+++ app/graphql/resolvers/employees/list_subordinates.rb
@@ -5,14 +5,18 @@ module Resolvers
     class ListSubordinates < GraphQL::Schema::Resolver
       description 'List all Employees managed by an Employee'
 
-      argument :id, ID, required: true
+      argument :employee_id, ID, required: true
+      argument :second_level, Boolean, required: false
 
       type Types::EmployeeType.connection_type, null: false
 
-      def resolve(id:)
-        employee = Employee.find(id)
+      def resolve(employee_id:, second_level: false)
+        employee = Employee.find(employee_id)
 
-        employee.subordinates
+        return employee.subordinates unless second_level
+
+        # Retrieve all subordinates of employee subordinates in one single list
+        employee.subordinates.flat_map(&:subordinates)
       rescue ActiveRecord::RecordNotFound => e
         GraphQL::ExecutionError.new(e)
       end
